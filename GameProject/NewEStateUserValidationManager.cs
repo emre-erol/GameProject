d184b109520c4a40ac5c2cf86ebb8063da196dd0
emre-erol <erol.emre@outlook.com>
2021-01-31 23:13:42 +0300

    Add project files.

---
+++ GameProject/NewEStateUserValidationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class NewEStateUserValidationManager : IUserValidationService
+    {
+        public bool ValiDate(Gamer gamer)
+        {
+            return true;
+        }
+    }
+}
---
+++ GameProject/UserValidationManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool ValiDate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 1994 && gamer.FirstName == "Benjamin" && gamer.LastName == "Onwuachi" && gamer.IdentityNumber==12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
